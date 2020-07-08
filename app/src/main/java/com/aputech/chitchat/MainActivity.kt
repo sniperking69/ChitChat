5fa15ddfcbff382ccfcbaba5b673f670a409ae84
adann <adan.nelson28@gmail.com>
2020-07-09 03:50:31 +0500

    tab layout pager adapter

--- app/src/main/java/com/aputech/chitchat/MainActivity.kt
+++ app/src/main/java/com/aputech/chitchat/MainActivity.kt
@@ -1,50 +1,48 @@
 package com.aputech.chitchat
 
 import android.os.Bundle
-import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tool:Toolbar=findViewById(R.id.toolbar);
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        setSupportActionBar(tool)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val toolbar:Toolbar= findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+//        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+//        val viewPager: ViewPager = findViewById(R.id.view_pager)
+//        viewPager.adapter = sectionsPagerAdapter
+//        val tabs: TabLayout = findViewById(R.id.tabs)
+//        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu,menu);
+        menuInflater.inflate(R.menu.main_menu,menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
-            R.id.searchMsg -> {
-                Toast.makeText(this,"This is a Toast",Toast.LENGTH_SHORT).show()
-                true
+            R.id.searchMsg ->{
+                Toast.makeText(this,"This is my message",Toast.LENGTH_SHORT).show()
+                return true
             }
-            else -> super.onOptionsItemSelected(item)
+            else-> super.onOptionsItemSelected(item)
         }
     }
 }
--- app/src/main/java/com/aputech/chitchat/SplashScreen.kt
+++ app/src/main/java/com/aputech/chitchat/SplashScreen.kt
@@ -1,11 +1,18 @@
 package com.aputech.chitchat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        val In: Intent = Intent(this,MainActivity::class.java)
+        Handler(Looper.myLooper()!!).postDelayed(Runnable {
+            startActivity(In)
+        },3000)
     }
 }
